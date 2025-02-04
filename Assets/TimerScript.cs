a5d6a630771befc0c3d269cc33a5fe84d615fdf9
buildingtub723 <sebastianfacundo2019@gmail.com>
2025-02-04 14:25:22 -0500

    Latest upload
    simple touches minor for peru review

---
+++ Assets/TimerScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using TMPro; 
+
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    float elapsedTime;
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        int minutes = Mathf.FloorToInt(elapsedTime / 60);
+        int seconds = Mathf.FloorToInt(elapsedTime % 60);
+        timerText.text = string.Format("{0:00}:{1:00}", minutes,seconds);
+
+        //timerText.text = elapsedTime.ToString();
+    }
+}
